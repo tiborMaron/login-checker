4c5ca7038fab6c48fa73fbed79998f828098e80a
Unknown <tibormaron@gmail.com>
2018-11-28 22:57:51 +0100

    added loading icon

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
 
     $('.toggler').on('click', null, function(){
+
+        // Toggle result page
         $(this).parent().parent().find('.results').slideToggle();
+
+        // Get credentials
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        // AJAX calls
+        var platforms = $(".platform-container");
+        $.each(platforms, function () {
+            $(this).find('.fa-circle').removeClass("far fa-circle").addClass("fas fa-spinner loading");
+
+
+        });
 
     });
 
