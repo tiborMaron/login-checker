18b36a2c5704beec461c96741038819d006bb072
Unknown <tibormaron@gmail.com>
2018-11-29 11:20:38 +0100

    finished design

--- src/main/java/com/codecool/loginchecker/api/CheckerControllerREST.java
+++ src/main/java/com/codecool/loginchecker/api/CheckerControllerREST.java
@@ -14,15 +14,13 @@ import java.io.IOException;
 public class CheckerControllerREST {
 
     @PostMapping("/api/{platform}")
-    public String checkPlatform(@PathVariable("platform") String platform, HttpServletRequest request) throws IOException {
+    public boolean checkPlatform(@PathVariable("platform") String platform, HttpServletRequest request) throws IOException {
         Gson gson = new Gson();
         Type type = new TypeToken<Map<String, String>>() {
         }.getType();
         Map<String, String> credentials = gson.fromJson(request.getReader(), type);
 
-        System.out.println(platform);
-
-        return "valami";
+        
     }
 
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -12,28 +12,41 @@ $(document).ready(function () {
 
         // AJAX calls
         var platforms = $(".platform-container");
+        var numOfSuccess = 0;
         $.each(platforms, function () {
             var platform = $(this);
             platform.find('.fa-circle').removeClass("far fa-circle").addClass("fas fa-spinner loading");
 
             var path = "/api/" + this.id;
             $.ajax(path, {
+                async: false,
                 type: 'POST',
                 dataType: 'json',
                 contentType: 'application/json; charset=utf-8',
                 data: JSON.stringify(credentials),
 
-                success: function (resultData) {
-                    platform.find('.fa-spinner').removeClass("fas fa-spinner loading").addClass("fas fa-check-square");
+                success: function (result) {
+                    if (result === true) {
+                        platform.find('.fa-spinner').removeClass("fas fa-spinner loading").addClass("fas fa-check-square");
+                        platform.find('.iconstatusmessage').text("SUCCESS").css("color", "green");
+                        numOfSuccess++;
+                    } else {
+                        platform.find('.fa-spinner').removeClass("fas fa-spinner loading").addClass("fas fa-window-close");
+                        platform.find('.iconstatusmessage').text("NOT FOUND").css("color", "red");
+                    }
                 },
 
                 error: function () {
-                    platform.find('.fa-spinner').removeClass("fas fa-spinner loading").addClass("fas fa-times");
+                    platform.find('.fa-spinner').removeClass("fas fa-spinner loading").addClass("fas fa-circle");
+                    platform.find('.iconstatusmessage').text("ERROR");
                 }
             })
 
         });
 
+        // Show summary page
+        $(this).parent().parent().find('.summary').slideToggle();
+        $("#summary").text("Found " + numOfSuccess + " matches!");
     });
 
 });
