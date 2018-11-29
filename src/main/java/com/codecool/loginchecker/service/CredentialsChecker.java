4580d46e21f292c04770094a6fe4fb5a7b7c4d49
Milan Kiszely <milankiszely@gmail.com>
2018-11-29 10:51:23 +0100

    Added gmail login

--- src/main/java/com/codecool/loginchecker/service/CredentialsChecker.java
+++ src/main/java/com/codecool/loginchecker/service/CredentialsChecker.java
@@ -1,10 +1,18 @@
 package com.codecool.loginchecker.service;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class CredentialsChecker {
@@ -17,6 +25,30 @@ public class CredentialsChecker {
     }
 
     public boolean gmail(String email, String password){
+        ChromeDriver driver = new ChromeDriver();
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            Actions actions = new Actions(driver);
+
+            driver.get("https://accounts.google.com/signin");
+
+            WebElement identifierInput = wait.until(ExpectedConditions.elementToBeClickable(By.name("identifier")));
+            actions.moveToElement(identifierInput).sendKeys(email);
+            driver.findElementById("identifierNext").click();
+
+            WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.name("password")));
+            actions.moveToElement(passwordInput).sendKeys(password);
+            driver.findElementById("passwordNext").click();
+
+            wait.until(ExpectedConditions.urlContains("myaccount"));
+            if (driver.manage().getCookieNamed("APISID") != null)
+                return true;
+
+        } catch (TimeoutException e) {
+            return false;
+        } finally {
+            driver.close();
+        }
         return false;
     }
 
